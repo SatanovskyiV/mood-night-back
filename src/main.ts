8cb36660f07c3999273e5eacc3f4b4fdc5ecdebd
Vitalii Satanovskyi <satanovskiy.vitaliy95@gmail.com>
2022-11-26 20:26:06 +0200

    Connected Postgres DB

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  console.log('Starting up...........');
+
+  if (!process.env.POSTGRES_PASSWORD) {
+    throw new Error('POSTGRES_PASSWORD must be defined');
+  }
+
   const app = await NestFactory.create(AppModule);
   await app.listen(4000);
 }
