02894d3abc5d7d9751e0ace04ee93283aede5692
Vitalii Satanovskyi <satanovskiy.vitaliy95@gmail.com>
2022-11-27 16:26:41 +0200

    Were added posts

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -10,18 +10,12 @@ import {
 // import { Report } from '../reports/report.entity';
 
 @Entity()
-export class User {
+export class Post {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
-  email: string;
-
-  @Column()
-  password: string;
-
-  @Column({ default: true })
-  admin: boolean;
+  title: string;
 
   // @OneToMany(() => Report, (report) => report.user)
   // reports: Report[];
